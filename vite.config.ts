be6a9de0a9078a11cf5a613cac7495f404b4b6f1
roysadeh520 <roysadeh520@gmail.com>
2026-02-06 12:59:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Investments/',
+  build: {
+    outDir: 'dist', // ודא שהתוצר הולך לתיקייה שה-YAML מחפש
+  }
 })
