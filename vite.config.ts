39ef928ebf11de089ae77d108b689d5241587c53
roysadeh520 <157304431+roysadeh520@users.noreply.github.com>
2026-02-06 12:46:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/Investments/', // שם הריפוזיטורי שלך בין סלאשים
+  plugins: [react()], // או מה שיש לך שם
 })
